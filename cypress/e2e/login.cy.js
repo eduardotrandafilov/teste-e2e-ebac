29b0537282e19c46fb6b3dcf7bb64d8d6cdc052b
Eduardo <eduardo.trandafilov@gmail.com>
2024-09-18 14:25:30 -0300

    inclusao lista

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -17,20 +17,20 @@ context('Funcionalidade Login', () => {
     });
 
     it('Login com sucesso usando Comando customizado', () => {
-        cy.login(dadosLogin.usuario, dadosLogin.senha)
+        cy.login(dadosLogin[1].usuario, dadosLogin[1].senha)
         cy.get('.page-title').should('contain', 'Minha conta')
     });
 
     it('Login usando fixture', () => {
         cy.fixture('perfil').then((dados) => {
-            cy.login(dados.usuario, dados.senha)
+            cy.login(dados[1].usuario, dados[1].senha)
         })
         cy.get('.page-title').should('contain', 'Minha conta')
     });
 
     it.skip('Deve fazer login com sucesso - sem otimização', () => {
-        cy.get('#username').type(dadosLogin.usuario)
-        cy.get('#password').type(dadosLogin.senha, { log: false })
+        cy.get('#username').type(dadosLogin[1].usuario)
+        cy.get('#password').type(dadosLogin[1].senha, { log: false })
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, karli.rowe83 (não é karli.rowe83? Sair)')
